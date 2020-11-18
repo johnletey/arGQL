e108bdcde80c6ecd6a47e2e2c588c4b45cd0998c
John Letey <johnletey@gmail.com>
2020-11-18 19:03:59 +0000

    feat: add function that retrieves all results

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import fetch from "node-fetch";
+import GQLResultInterface, { GQLEdgeInterface } from "./types";
 
-export const query = async (
+export const run = async (
   query: string,
   variables?: Record<string, unknown>
-): Promise<any> => {
+): Promise<GQLResultInterface> => {
   const graphql = JSON.stringify({
     query,
     variables,
@@ -19,4 +20,30 @@ export const query = async (
 
   const res = await fetch("https://arweave.net/graphql", requestOptions);
   return await res.clone().json();
+};
+
+export const all = async (
+  query: string,
+  variables?: Record<string, unknown>
+): Promise<any> => {
+  let hasNextPage = true;
+  let edges: GQLEdgeInterface[] = [];
+  let cursor: string = "";
+
+  while (hasNextPage) {
+    const res = (
+      await run(query, {
+        ...variables,
+        cursor,
+      })
+    ).data.transactions;
+
+    if (res.edges && res.edges.length) {
+      edges = edges.concat(res.edges);
+      cursor = res.edges[res.edges.length - 1].cursor;
+    }
+    hasNextPage = res.pageInfo.hasNextPage;
+  }
+
+  return edges;
 };
