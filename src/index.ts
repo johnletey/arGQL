9a1fdf1818a0f6eae064ec6facd540e48977a07a
John Letey <johnletey@gmail.com>
2020-11-28 11:00:52 +0000

    feat: cache mined tx info

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,21 @@ export const all = async (
 };
 
 export const tx = async (id: string): Promise<GQLNodeInterface> => {
+  const isBrowser: boolean = typeof window !== "undefined";
+
+  if (isBrowser) {
+    const cache = JSON.parse(localStorage.getItem("gqlCache") || "{}");
+    if (id in cache) return JSON.parse(cache[id]);
+  }
+
   const res = await run(txQuery, { id });
+
+  if (isBrowser && res.data.transaction.block) {
+    const cache = JSON.parse(localStorage.getItem("gqlCache") || "{}");
+    cache[id] = res.data.transaction;
+    localStorage.setItem("gqlCache", JSON.stringify(cache));
+  }
+
   return res.data.transaction;
 };
 
