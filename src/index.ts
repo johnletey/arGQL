208dec19c0035a32d50f6337996de7961fe10953
Ros McMahon <rosmcmahon@gmail.com>
2023-02-22 22:47:20 +0700

    feat: convert index to `arGql` closure that takes `endpointUrl` argument. `axios`=>`fetch`. remove optional `gqlUrl` from all functions.

--- src/index.ts
+++ src/index.ts
@@ -2,89 +2,105 @@ import GQLResultInterface, {
   GQLEdgeInterface,
   GQLNodeInterface,
 } from "./faces";
-import axios from "axios";
 import txQuery from "./queries/tx";
 
-let GQL_ENDPOINT = "https://arweave.net/graphql" //default
-export const setEndpointUrl = (full_GQL_Url: string) => GQL_ENDPOINT = full_GQL_Url
+export function arGql(endpointUrl?: string){
+  //sanity check
+  if(endpointUrl && !endpointUrl.match(/^https?:\/\/.*\/graphql$/)){
+    throw new Error(`string doesn't appear to be a URL of the form <http(s)://some-domain/graphql>'. You entered "${endpointUrl}"`)
+  }
+  const _endpointUrl = endpointUrl || 'https://arweave.net/graphql' //default to arweave.net/graphql 
 
-export const run = async (
-  query: string,
-  variables?: Record<string, unknown>,
-  gqlUrl?: string,
-): Promise<GQLResultInterface> => {
-  const graphql = JSON.stringify({
-    query,
-    variables,
-  });
-
-  const { data: res } = await axios.post(
-    gqlUrl || GQL_ENDPOINT,
-    graphql,
-    {
+  const run = async (
+    query: string,
+    variables?: Record<string, unknown>,
+  ): Promise<GQLResultInterface> => {
+    const graphql = JSON.stringify({
+      query,
+      variables,
+    });
+  
+    const { json, ok, status, statusText } = await fetch(_endpointUrl, {
+      method: 'POST',
       headers: {
-        "content-type": "application/json",
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
       },
+      body: graphql,
+    });
+
+    if(!ok){ 
+      throw new Error(`(${status}) ${statusText}`)
     }
-  );
+  
+    return await json();
+  };
 
-  return res;
-};
-
-export const all = async (
-  query: string,
-  variables?: Record<string, unknown>,
-  gqlUrl?: string,
-): Promise<GQLEdgeInterface[]> => {
-  let hasNextPage = true;
-  let edges: GQLEdgeInterface[] = [];
-  let cursor: string = "";
-
-  while (hasNextPage) {
-    const res = (
-      await run(
-        query, 
-        { ...variables, cursor },
-        gqlUrl,
-      )
-    ).data.transactions;
-
-    if (res.edges && res.edges.length) {
-      edges = edges.concat(res.edges);
-      cursor = res.edges[res.edges.length - 1].cursor;
+  const all = async (
+    query: string,
+    variables?: Record<string, unknown>,
+  ): Promise<GQLEdgeInterface[]> => {
+    let hasNextPage = true;
+    let edges: GQLEdgeInterface[] = [];
+    let cursor: string = "";
+  
+    while (hasNextPage) {
+      const res = (
+        await run(
+          query, 
+          { ...variables, cursor },
+        )
+      ).data.transactions;
+  
+      if (res.edges && res.edges.length) {
+        edges = edges.concat(res.edges);
+        cursor = res.edges[res.edges.length - 1].cursor;
+      }
+      hasNextPage = res.pageInfo.hasNextPage;
     }
-    hasNextPage = res.pageInfo.hasNextPage;
+  
+    return edges;
+  };
+
+  const tx = async (id: string): Promise<GQLNodeInterface> => {
+    const isBrowser: boolean = typeof window !== "undefined";
+  
+    if (isBrowser) {
+      const cache = JSON.parse(localStorage.getItem("gqlCache") || "{}");
+      if (id in cache) return JSON.parse(cache[id]);
+    }
+  
+    const res = await run(txQuery, { id });
+  
+    if (isBrowser && res.data.transaction.block) {
+      const cache = JSON.parse(localStorage.getItem("gqlCache") || "{}");
+      cache[id] = res.data.transaction;
+      localStorage.setItem("gqlCache", JSON.stringify(cache));
+    }
+  
+    return res.data.transaction;
+  };
+  
+  
+  const fetchTxTag = async (
+    id: string,
+    name: string,
+  ): Promise<string | undefined> => {
+    const res = await tx(id);
+
+    const tag = res.tags.find((tag) => tag.name === name);
+    if (tag) return tag.value;
+  };
+  
+  return {
+    run,
+    all,
+    tx,
+    fetchTxTag,
+    getConfig: ()=> {
+      return { 
+        endpointUrl: _endpointUrl, 
+      }
+    }
   }
-
-  return edges;
-};
-
-export const tx = async (id: string, gqlUrl?: string): Promise<GQLNodeInterface> => {
-  const isBrowser: boolean = typeof window !== "undefined";
-
-  if (isBrowser) {
-    const cache = JSON.parse(localStorage.getItem("gqlCache") || "{}");
-    if (id in cache) return JSON.parse(cache[id]);
-  }
-
-  const res = await run(txQuery, { id }, gqlUrl);
-
-  if (isBrowser && res.data.transaction.block) {
-    const cache = JSON.parse(localStorage.getItem("gqlCache") || "{}");
-    cache[id] = res.data.transaction;
-    localStorage.setItem("gqlCache", JSON.stringify(cache));
-  }
-
-  return res.data.transaction;
-};
-
-export const fetchTxTag = async (
-  id: string,
-  name: string,
-  gqlUrl?: string
-): Promise<string | undefined> => {
-  const res = await tx(id, gqlUrl);
-
-  const tag = res.tags.find((tag) => tag.name === name);
-  if (tag) return tag.value;
-};
+}
