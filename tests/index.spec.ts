660d6d936671f85009f7eba900d5c8ecb83556b9
Ros McMahon <rosmcmahon@gmail.com>
2023-02-23 09:13:44 +0700

    task: tests for all functions

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,6 +2,35 @@ import { expect } from 'chai'
 import {  } from 'mocha'
 import { arGql } from '../src/index'
 
+
+const testQuery = `query($cursor: String) {
+  transactions(
+    # your query parameters
+    
+		tags: [
+			{ name: "App-Name", values: ["CommunityXYZ"] }
+		]
+
+    # standard template below
+    after: $cursor
+    first: 100
+  ) {
+    pageInfo {
+      hasNextPage
+    }
+    edges {
+      cursor
+      node {
+        # what tx data you want to query for:
+        
+				id
+
+      }
+    }
+  }
+}`
+
+
 describe('ar-gql tests', function(){
 
 	it('should initialise default and other instances of arGql', async()=> {
@@ -14,15 +43,41 @@ describe('ar-gql tests', function(){
 
 		const badUrl = 'arweave.net'
 		try{
-			arGql(badUrl)
+			const badGql = arGql(badUrl)
 			expect(true, 'no error was thrown with badUrl').false //should not get here
 		}catch(e:any){
 			expect(e.message).eq(`string doesn't appear to be a URL of the form <http(s)://some-domain/graphql>'. You entered "${badUrl}"`)
 		}
 	})
 
-	// it('should execute `run` successfully', async()=> {
-	// 	const res = await 
-	// })
+	it('should execute `run` successfully', async()=> {
+		const argql = arGql()
+		const res = await argql.run(testQuery) 
+		expect(res.data.transactions.edges.length).to.be.greaterThan(0)
+	})
+
+	it('should execute `all` and retrieve several pages successfully', async function() {
+		this.timeout(10_000)
+
+		const argql = arGql()
+		const edges = await argql.all(testQuery) 
+		expect(edges.length).to.be.greaterThan(0)
+	})
+
+	it('should execute `tx` successfully', async()=> {
+		const argql = arGql()
+		const txid = 'DeYQPjoEQLLds7usOMZFJFCe7VyTpodYl6Mok6UP6Z4'
+		const txMeta = await argql.tx(txid) 
+		expect(txMeta.id).eq(txid)
+	})
+
+	it('should execute `fetchTxTag` successfully', async()=> {
+		const argql = arGql()
+		const txid = 'DeYQPjoEQLLds7usOMZFJFCe7VyTpodYl6Mok6UP6Z4'
+		const tag = { name: 'Content-Type', value: 'text/plain'}
+		const txMeta = await argql.fetchTxTag(txid, tag.name) 
+		expect(txMeta).eq(tag.value)
+	})
+
 
 })
